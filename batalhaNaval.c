b13377c5121ddb7b798eabb709fe43958e798a4f
Sidney Vasconcelos <sidneyvasconcelos11@gmail.com>
2025-10-21 01:32:53 +0000

    criado um tabuleiro simplificado e posicionei dois navios nele: um na vertical e outro na horizontal.

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+#define tamanhoTabuleiro 10
+#define tamanhoNavio 3
+
 // Desafio Batalha Naval - MateCheck
 // Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
 // Siga os comentários para implementar cada parte do desafio.
@@ -35,6 +38,55 @@ int main() {
     // 0 0 1 0 0
     // 1 1 1 1 1
     // 0 0 1 0 0
+
+    int tabuleiro[tamanhoTabuleiro][tamanhoTabuleiro] = {0}; // Legenda para o Tabuleiro:  0 = água, 3 = navio
+
+     // Coordenadas dos navios
+    int linhaNavioHorizontal = 2; // linha inicial do navio na horizontal
+    int colunaNavioHorizontal = 4; // coluna inicial do navio na horizontal
+    
+    int linhaNavioVertical = 5; // linha inicial do navio na vertical
+    int colunaNavioVertical = 7; // coluna inicial do navio na vertical
+
+    // POSIÇÃO DO NAVIO NA HORIZONTAL
+    if (colunaNavioHorizontal + tamanhoNavio <= tamanhoTabuleiro) {
+        for (int i = 0; i < tamanhoNavio; i++) {
+            // Marca a posição do navio com 3
+            tabuleiro[linhaNavioHorizontal][colunaNavioHorizontal + i] = 3;
+        }
+    } else {
+        printf("Navio horizontal ultrapassa os limites do tabuleiro.\n");
+        return 1;
+    }
+
+    // POSIÇÃO DO NAVIO NA VERTICAL
+    if (linhaNavioVertical + tamanhoNavio <= tamanhoTabuleiro) {
+        for (int i = 0; i < tamanhoNavio; i++) {
+            // verificar antes se já há outro navio no local.
+            if (tabuleiro[linhaNavioVertical + i][colunaNavioVertical] == 3) {
+                printf("Erro: navio vertical se sobrepõe ao navio horizontal.\n");
+                return 1;
+            }
+            tabuleiro[linhaNavioVertical + i][colunaNavioVertical] = 3;
+        }
+    } else {
+        printf("Navio vertical ultrapassa os limites do tabuleiro.\n");
+        return 1;
+    }
+
+    // EXIBIÇÃO DO TABULEIRO
+    printf("===============================\n");
+    printf("=== TABULEIRO BATALHA NAVAL ===\n");
+    printf("===============================\n\n");
+    for (int linha = 0; linha < tamanhoTabuleiro; linha++) {
+        for (int coluna = 0; coluna < tamanhoTabuleiro; coluna++) {
+            printf("%d ", tabuleiro[linha][coluna]);
+        }
+        printf("\n");
+    }
+
+    printf("\nLegenda: 0 = Água | 3 = Navio\n");
+    printf("===============================\n\n");
 
     return 0;
 }
